e88df1c1f8394ec94e5f7f3d5c6ccd8c81afc3b6
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-23 22:21:59 +0100

    vuex utility directory

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex';
+import animeModule from './modules/anime.module'
+
+export default createStore({
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {
+        animeModule
+    }
+})
