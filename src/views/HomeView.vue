d35eaa34c86fddd0c272534e8a72a5d4d3dadf8b
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-23 22:22:50 +0100

    Renamed file [Home -> HomeView]

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar'
+
+export default {
+    name: 'Home',
+    components: {  
+        NavBar,
+    },
+    setup() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
