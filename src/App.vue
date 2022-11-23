d14bf8ddd04a448a64ad151d26fb3eacdc0dfdf8
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-23 22:18:48 +0100

    Adding Navbar component and router-view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  
+  <header>
+    <NavBar />
+  </header>
+  <router-view />
 </template>
 
 <script>
+import NavBar from '@/components/NavBar'
 
 export default {
-  name: 'App',
-  components: {
-    
-  }
+    name: 'App',
+    components: {  
+        NavBar,
+    }
 }
 </script>
 
