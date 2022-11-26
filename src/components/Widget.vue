c4e5d3cd620a92c2342189d81cddcccb1d68b7f3
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-26 15:33:57 +0100

    Widget component

---
+++ src/components/Widget.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="widget">
+        <div class="widget-top">
+            <h1>Popular</h1>
+            <h3>view all</h3>
+        </div>
+        <div class="widget-content">
+            <img src="" alt="">
+            <h3>Title</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
