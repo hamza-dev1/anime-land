9d2ce8ee014de022aa26d60c260b1a86954e408e
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-26 15:34:41 +0100

    Refactoring design

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <header>
-    <NavBar />
-  </header>
-  <router-view />
+    <header class="overflow-hidden">
+        <NavBar />
+    </header>
+    <div class="wrapper mx-auto">
+        <router-view />
+    </div>
 </template>
 
 <script>
-import NavBar from '@/components/NavBar'
+import NavBar from "@/components/NavBar";
 
 export default {
-    name: 'App',
-    components: {  
+    name: "App",
+    components: {
         NavBar,
-    }
-}
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
